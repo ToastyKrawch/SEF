855355b5562f8358e0cbca5e984f3ed7aad2d3f8
Bradley Krawchyk <bkrawchyk4812@conestogac.on.ca>
2022-12-07 22:45:49 -0500

    Added second feature

---
+++ classifier.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void train()
+{
+	printf("train");
+	printf("The first feature is added");
+}
+
+void predict()
+{
+	printf("predict");
+	printf("The second feature is added");
+}
